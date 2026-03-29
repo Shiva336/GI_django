ca30c42c35c334a8a733c1f3f520c011c1c0d280
Shiva336 <shivasundar911@gmail.com>
2026-03-29 16:10:27 +0530

    unit tests folder structure and initial setup

---
+++ users/tests/test_api.py
@@ -0,0 +1,6 @@
+import io
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+class UserCSVUploadAPITest(APITestCase):
+    pass
---
+++ users/tests/test_services.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+from users.services import process_csv
+from users.models import User
+
+class ProcessCSVTest(TestCase):
+    pass
